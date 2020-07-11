5272f7bdeeb95c7bd08121246d1b5b0740980c1c
Malte2036 <18309412+Malte2036@users.noreply.github.com>
2020-07-12 00:27:05 +0200

    Naming fixed

--- lib/utils/ownMath.dart
+++ lib/utils/ownMath.dart
@@ -1,17 +1,17 @@
 import 'dart:math' show pow;
 
 class OwnMath {
-  static List<int> _byteWerte = [
+  static List<int> _byteValues = [
     pow(10, 0),
     pow(10, 3),
     pow(10, 6),
     pow(10, 9),
     pow(10, 12),
   ];
-  static List<String> _byteString = ["B", "kB", "MB", "GB", "TB"];
+  static List<String> _byteString = ["B", "KB", "MB", "GB", "TB"];
   static String bytesToHumanString(int bytes) {
-    for (int i = (_byteWerte.length - 1); i >= 0; i--) {
-      int sizeType = _byteWerte[i];
+    for (int i = (_byteValues.length - 1); i >= 0; i--) {
+      int sizeType = _byteValues[i];
       if (sizeType < bytes) {
         return round(bytes / sizeType).toString() + _byteString[i];
       }
@@ -20,8 +20,8 @@ class OwnMath {
   }
 
   static String bytesToHumanCompareString(int smallByte, int bigByte) {
-    for (int i = (_byteWerte.length - 1); i >= 0; i--) {
-      int sizeType = _byteWerte[i];
+    for (int i = (_byteValues.length - 1); i >= 0; i--) {
+      int sizeType = _byteValues[i];
       if (sizeType < bigByte) {
         return round(smallByte / sizeType).toString() +
             "/" +
