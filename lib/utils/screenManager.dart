d9fa7e3e07039ce038775cd89ab8c153ac369512
Malte2036 <18309412+Malte2036@users.noreply.github.com>
2020-08-02 22:36:44 +0200

    Changed screenManager

--- lib/utils/screenManager.dart
+++ lib/utils/screenManager.dart
@@ -16,17 +16,15 @@ class ScreenManager {
     Size screenSize = MediaQuery.of(context).size;
     //print("Screen Width: " + screenSize.width.toString());
     if (screenSize.height > screenSize.width) {
-      if (screenSize.width >= 500)
-        return 2;
-      else
-        return 1;
+      if (screenSize.width >= 1000) return 3;
+      if (screenSize.width >= 500) return 2;
+      return 1;
     } else {
-      if (screenSize.width >= 1000)
-        return 4;
-      else if (screenSize.width >= 700)
-        return 3;
-      else
-        return 2;
+      if (screenSize.width >= 2400) return 6;
+      if (screenSize.width >= 1700) return 5;
+      if (screenSize.width >= 1000) return 4;
+      if (screenSize.width >= 700) return 3;
+      return 2;
     }
   }
 
