ba6da94d34fd37351c6333acfbfe6f42b4574b25
Malte2036 <18309412+Malte2036@users.noreply.github.com>
2020-08-13 10:33:31 +0200

    Improved: server_listener.py timeout

--- scripts/server_listener.py
+++ scripts/server_listener.py
@@ -1,24 +1,33 @@
 import asyncio
+import time
 import websockets
 import daemon
 from termcolor import colored
 
 import system_data
 
+t = 0.0
+
 async def echo(websocket, path):
     async for message in websocket:
         await handleMessage(message, websocket)
 
 async def handleMessage(message, websocket):
+    global t
+
     print(message)
     if(message == "getSystemData()"):
         await sendSystemData(websocket, system_data.getData())
     elif(message == "getSystemDetailData()"):
+        if(time.time() < t):
+            print("Timeout")
+            return
+        
         await sendSystemData(websocket, system_data.getDetailData())
     else:
         print(colored("message not valid!", 'red'))
-
-    await asyncio.sleep(2)
+    
+    t = time.time() + 1.5
 
 async def sendSystemData(websocket, data):
     await websocket.send(data)
@@ -28,8 +37,6 @@ def run_daemon():
     asyncio.get_event_loop().run_until_complete(
         websockets.serve(echo, '0.0.0.0', 9499))
     asyncio.get_event_loop().run_forever()
-
-
 
 print(colored('starting websocket server at ws://localhost:9499...', 'green'))
 print()
